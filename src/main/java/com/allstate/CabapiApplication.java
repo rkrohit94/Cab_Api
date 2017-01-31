2055f5cc128a2a654c089cf65af55929dc13822d
lavanyareddygowlla <lredg@allstate.com>
2017-01-31 10:15:36 +0530

    initial setup

---
+++ src/main/java/com/allstate/CabapiApplication.java
@@ -0,0 +1,12 @@
+package com.allstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabapiApplication.class, args);
+	}
+}
